af979fa906bac0592db11d9f478a8b613d148283
MykolaiPcholin <nickolay.pcholin@protonmail.com>
2023-05-20 15:16:53 +0300

    simple project structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function showMessage(str) {
+    console.log(`Hello there, ${str}! :)`)
+  }
+  let string = "Ben";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Init title</h1>
+      <img src='https://lh3.googleusercontent.com/blogger_img_proxy/AByxGDTWOAcYCJ6BDI3AtISL-AAxTdrD17pig6WXJMdLTfJI7RcUyF-eF7Ohblmr_66Q39-ukvbJBwNzhoRcZnRj7G5ywC3xdkSNVkMNJRrhWmaVq1roSBxh0BC0DcYkdDAvdpJw5oKnvF6URN-rtIRi=w919-h516-p-k-no-nu' alt='Background img'/>    
+      <br/>
+      <button className='button' onClick={showMessage(string)}>I`m not working</button>
+      <button className='button' onClick={() => showMessage(string)}>Click me, I`m working</button>
     </div>
   );
 }
